a2d910814eb04f7b28f4b0e9c10b03a3e152ef0e
Alexey.Bosalyga <alexey.bosalyga@billing.ru>
2024-09-18 13:43:48 +0400

    lesson3 additional tasks

--- src/main/java/org/skillsmart/lesson3/MultidimensionalDynArray.java
+++ src/main/java/org/skillsmart/lesson3/MultidimensionalDynArray.java
@@ -5,24 +5,12 @@ import java.lang.reflect.Array;
 public class MultidimensionalDynArray<T> {
 
     private static final int APPEND_MULTIPLIER = 2;
-    private static final double REDUCE_MULTIPLIER = 1.5;
-    private static final double EMPTY_RATIO = 0.5;
 
-    //public T[] array;
     public Object[] array;
     public int count;
-    public int capacity;
     public int dimensions;
     public int[] capacities;
-    public int[] counts;
     Class clazz;
-
-    public MultidimensionalDynArray(Class clz)
-    {
-        clazz = clz;
-        count = 0;
-        //makeArray(1, INITIAL_CAPACITY);
-    }
 
     public MultidimensionalDynArray(Class clz, int... initialCapacities)
     {
@@ -30,7 +18,6 @@ public class MultidimensionalDynArray<T> {
         dimensions = initialCapacities.length;
         array = makeArray(initialCapacities, 0);
         capacities = initialCapacities;
-        //array = (T[]) makeArray(initialCapacities, 0);
     }
 
     public Object[] makeArray(int[] initialCapacities, int dimension) {
@@ -39,7 +26,6 @@ public class MultidimensionalDynArray<T> {
             return lastDimension;
         }
         Object[] currentDimension = (Object[]) Array.newInstance(Object.class, initialCapacities[dimension]);
-        //Object[] currentDimension = (Object[]) Array.newInstance(SimpleDynArray.class, initialCapacities[dimension]);
         for (int i = 0; i < initialCapacities[dimension]; i++) {
             currentDimension[i] = makeArray(initialCapacities, dimension + 1);
         }
@@ -48,9 +34,9 @@ public class MultidimensionalDynArray<T> {
 
     public T getItem(int... indices)
     {
-        /*if (index.length < count) {
-            throw new ArrayIndexOutOfBoundsException();
-        }*/
+        if (indices.length < dimensions) {
+            throw new ArrayStoreException();
+        }
         return (T) getValue(array, 0, indices);
     }
 
@@ -58,10 +44,14 @@ public class MultidimensionalDynArray<T> {
         if (level == indices.length - 1) {
             return currentArray[indices[level]];
         }
-        return getValue((Object[]) currentArray[level + 1], level + 1, indices);
+        return getValue((Object[]) currentArray[indices[level]], level + 1, indices);
     }
 
     public void setItem(T value, int... indices) {
+        if (indices.length < dimensions) {
+            throw new ArrayStoreException();
+        }
+        resizeArrayIfNeed(indices);
         //надо в цикле найти последовательно конечную точку в массиве массивов массивов и т.д...
         setValue(array, value, 0, indices);
         count += 1;
@@ -75,88 +65,29 @@ public class MultidimensionalDynArray<T> {
         }
     }
 
-}
-
-class SimpleDynArray<T>
-{
-    private static final int APPEND_MULTIPLIER = 2;
-    private static final double REDUCE_MULTIPLIER = 1.5;
-    private static final double EMPTY_RATIO = 0.5;
-    public T [] array;
-    public int count;
-    public int capacity;
-    public int initialCapacity;
-    Class clazz;
-
-    public SimpleDynArray(Class clz, int new_capacity)
-    {
-        clazz = clz;
-        count = 0;
-        initialCapacity = new_capacity;
-        makeArray(new_capacity);
-    }
-
-    public void makeArray(int new_capacity)
-    {
-        T[] oldArray = array;
-        array = (T[]) Array.newInstance(this.clazz, new_capacity);
-        if (count > 0) {
-            System.arraycopy(oldArray, 0, array, 0, count);
+    public void resizeArrayIfNeed(int[] indices) {
+        boolean isNeedResize = false;
+        int[] oldCapacities = capacities.clone();
+        for (int i = 0; i < dimensions; i++) {
+            if (capacities[i] <= indices[i]) {
+                capacities[i] = Math.max(indices[i] + 1, capacities[i] * APPEND_MULTIPLIER);
+                isNeedResize = true;
+            }
         }
-        capacity = new_capacity;
-    }
-
-    public T getItem(int index)
-    {
-        if (index < 0 || index >= count) {
-            throw new ArrayIndexOutOfBoundsException();
-        }
-        return array[index];
-    }
-
-    public void append(T itm)
-    {
-        if (count == capacity) {
-            makeArray(capacity * APPEND_MULTIPLIER);
-        }
-        array[count] = itm;
-        count += 1;
-    }
-
-    public void insert(T itm, int index)
-    {
-        if (index < 0 || index > count) {
-            throw new ArrayIndexOutOfBoundsException();
-        }
-        if (index == capacity || count == capacity) {
-            makeArray(capacity * APPEND_MULTIPLIER);
-        }
-        for (int i = count; i > index; i--) {
-            array[i] = array[i - 1];
-        }
-        array[index] = itm;
-        count += 1;
-    }
-
-    public void remove(int index)
-    {
-        if (index < 0 || index >= count) {
-            throw new ArrayIndexOutOfBoundsException();
-        }
-        shrinkIfNeed();
-        for (int i = index; i < count; i++) {
-            array[i] = array[i + 1];
-        }
-        count -= 1;
-    }
-
-    private void shrinkIfNeed() {
-        if (capacity == initialCapacity) {
+        if (!isNeedResize) {
             return;
         }
-        if (count - 1 < (int) (capacity * EMPTY_RATIO)) {
-            makeArray(Math.max((int) (capacity / REDUCE_MULTIPLIER), initialCapacity));
-        }
+        Object[] oldArray = array;
+        array = makeArray(capacities, 0);
+        copyArray(oldArray, array, 0, oldCapacities);
     }
 
+    private void copyArray(Object[] oldArray, Object[] currentArray, int level, int[] _capacities) {
+        if (level == _capacities.length - 1) {
+            System.arraycopy(oldArray, 0, currentArray, 0, oldArray.length);
+            return;
+        }
+        copyArray((Object[]) oldArray[_capacities[level] - 1], (Object[]) currentArray[_capacities[level] - 1], level + 1, _capacities);
+    }
 }
+
--- src/test/java/org/skillsmart/lesson3/MultidimensionalDynArrayTest.java
+++ src/test/java/org/skillsmart/lesson3/MultidimensionalDynArrayTest.java
@@ -7,13 +7,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class MultidimensionalDynArrayTest {
 
     @Test
-    void makeArray() {
-        MultidimensionalDynArray<Integer> array = new MultidimensionalDynArray<>(Integer.class, 2, 3);
-        int[][] arrayJava = new int[2][3];
-        assertEquals(4, arrayJava.length);
-    }
-
-    @Test
     void setItem() {
         MultidimensionalDynArray<Integer> array = new MultidimensionalDynArray<>(Integer.class, 2, 3);
         int[][] arrayJava = new int[2][3];
@@ -38,5 +31,63 @@ class MultidimensionalDynArrayTest {
         array.setItem(4562, 1, 2);
         assertEquals(6, array.count);
         assertEquals(4561, array.getItem(1, 1));
+    }
+
+    @Test
+    void setItem_3d() {
+        MultidimensionalDynArray<Integer> array = new MultidimensionalDynArray<>(Integer.class, 2, 2, 3);
+        int[][] arrayJava = new int[2][3];
+        array.setItem(0, 0, 0, 0);
+        array.setItem(1, 0, 0, 1);
+        array.setItem(2, 0, 0, 2);
+        array.setItem(3, 0, 1, 0);
+        array.setItem(4, 0, 1, 1);
+        array.setItem(5, 0, 1, 2);
+        array.setItem(6, 1, 0, 0);
+        array.setItem(7, 1, 0, 1);
+        array.setItem(8, 1, 0, 2);
+        array.setItem(9, 1, 1, 0);
+        array.setItem(10, 1, 1, 1);
+        array.setItem(11, 1, 1, 2);
+
+        assertEquals(12, array.count);
+    }
+
+    @Test
+    void getItem_3d() {
+        MultidimensionalDynArray<Integer> array = new MultidimensionalDynArray<>(Integer.class, 2, 2, 3);
+        array.setItem(0, 0, 0, 0);
+        array.setItem(1, 0, 0, 1);
+        array.setItem(2, 0, 0, 2);
+        array.setItem(3, 0, 1, 0);
+        array.setItem(4, 0, 1, 1);
+        array.setItem(5, 0, 1, 2);
+        array.setItem(6, 1, 0, 0);
+        array.setItem(7, 1, 0, 1);
+        array.setItem(8, 1, 0, 2);
+        array.setItem(9, 1, 1, 0);
+        array.setItem(10, 1, 1, 1);
+        array.setItem(11, 1, 1, 2);
+        assertEquals(5, array.getItem(0, 1, 2));
+        assertEquals(10, array.getItem(1, 1, 1));
+    }
+
+    @Test
+    void testSetItem_WithResize() {
+        MultidimensionalDynArray<Integer> array = new MultidimensionalDynArray<>(Integer.class, 1, 1);
+        array.setItem(123, 0, 0);
+        array.setItem(1231, 0, 1);
+        assertEquals(2, array.count);
+        assertEquals(1231, array.getItem(0, 1));
+    }
+
+    @Test
+    void testSetItem_3DWithResize() {
+        MultidimensionalDynArray<Integer> array = new MultidimensionalDynArray<>(Integer.class, 2, 2, 2);
+        array.setItem(123, 0, 0, 0);
+        array.setItem(1231, 0, 1, 0);
+        array.setItem(12312, 0, 1, 2);
+        assertEquals(3, array.count);
+        assertEquals(12312, array.getItem(0, 1, 2));
     }
 }
