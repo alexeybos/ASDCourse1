b688f8d246ddd264325108295dce117e590b08e0
Alexey.Bosalyga <alexey.bosalyga@billing.ru>
2024-09-28 20:51:48 +0400

    lesson7

--- src/main/java/org/skillsmart/lesson7/OrderedList.java
+++ src/main/java/org/skillsmart/lesson7/OrderedList.java
@@ -20,12 +20,14 @@ public class OrderedList<T>
 {
     public Node<T> head, tail;
     private boolean _ascending;
+    private int _count;
 
     public OrderedList(boolean asc)
     {
         head = null;
         tail = null;
         _ascending = asc;
+        _count = 0;
     }
 
     public int compare(T v1, T v2)
@@ -39,7 +41,7 @@ public class OrderedList<T>
             }
         }
         if (v1 instanceof String && v2 instanceof String) {
-            int result = ((String) v1).compareTo((String) v2);
+            int result = ((String) v1).trim().compareTo(((String) v2).trim());
             if (result < 0) {
                 return -1;
             }
@@ -52,29 +54,88 @@ public class OrderedList<T>
 
     public void add(T value)
     {
-        // автоматическая вставка value
-        // в нужную позицию
+        _count += 1;
+        Node<T> item = new Node<>(value);
+        if (head == null) {
+            this.head = item;
+            this.tail = item;
+            this.head.next = null;
+            this.head.prev = null;
+            return;
+        }
+        if ((_ascending && compare(this.head.value, value) >= 0) ||
+                (!_ascending && compare(value, this.head.value) >= 0)) { //new head value
+            item.next = this.head;
+            this.head.prev = item;
+            this.head = item;
+            return;
+        }
+        if ((_ascending && compare(value, this.tail.value) >= 0) ||
+                (!_ascending && compare(this.tail.value, value) >= 0)) { //new tail value
+            item.prev = this.tail;
+            this.tail.next = item;
+            this.tail = item;
+            return;
+        }
+        Node<T> iNode = this.head;
+        while ((_ascending && compare(value, iNode.value) > 0) || (!_ascending && compare(value, iNode.value) < 0)) {
+            iNode = iNode.next;
+        }
+        item.next = iNode;
+        iNode.prev.next = item;
+        item.prev = iNode.prev;
+        iNode.prev = item;
     }
 
     public Node<T> find(T val)
     {
-        return null; // здесь будет ваш код
+        Node<T> node = this.head;
+        for (int i = 0; i < this._count; i++) {
+            int compareResult = compare(node.value, val);
+            if (compareResult == 0) {
+                return node;
+            }
+            if ((_ascending && compareResult > 0) || (!_ascending && compareResult < 0)) {
+                return null;
+            }
+            node = node.next;
+        }
+        return null;
     }
 
     public void delete(T val)
     {
-        // здесь будет ваш код
+        if (_count == 0) {
+            return;
+        }
+        Node<T> node = find(val);
+        if (node != null) {
+            if (node.prev == null) {
+                this.head = node.next;
+            } else {
+                node.prev.next = node.next;
+            }
+            if (node.next == null) {
+                this.tail = node.prev;
+            } else {
+                node.next.prev = node.prev;
+            }
+            _count -= 1;
+        }
+
     }
 
     public void clear(boolean asc)
     {
         _ascending = asc;
-        // здесь будет ваш код
+        _count = 0;
+        this.head = null;
+        this.tail = null;
     }
 
     public int count()
     {
-        return 0; // здесь будет ваш код подсчёта количества элементов в списке
+        return _count;
     }
 
     ArrayList<Node<T>> getAll()
--- src/test/java/org/skillsmart/lesson7/OrderedListTest.java
+++ src/test/java/org/skillsmart/lesson7/OrderedListTest.java
@@ -7,6 +7,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class OrderedListTest {
 
     @Test
+    void testCompare() {
+        OrderedList list = new OrderedList<>(true);
+        assertEquals(-1, list.compare(1, 2));
+        assertEquals(1, list.compare(2, 1));
+        assertEquals(0, list.compare(2, 2));
+        assertEquals(-1, list.compare("qwerty", "werty"));
+        assertEquals(1, list.compare("werty", "qwerty"));
+        assertEquals(0, list.compare(" qwerty", "qwerty "));
+    }
+
+    @Test
     void testAdd_Asc() {
         OrderedList<Integer> list = new OrderedList<>(true);
         list.add(100);
@@ -19,10 +30,10 @@ class OrderedListTest {
         assertEquals(200, list.tail.value);
         list.add(150);
         assertEquals(100, list.head.value);
-        assertEquals(200, list.tail.value);
         assertEquals(150, list.head.next.value);
+        assertEquals(200, list.tail.value);
         assertEquals(3, list.count());
-        list.add(10);
+        list.add(10); //new head
         assertEquals(4, list.count());
         assertEquals(10, list.head.value);
         assertEquals(200, list.tail.value);
@@ -35,17 +46,24 @@ class OrderedListTest {
         assertEquals(120, list.head.next.next.value);
         assertEquals(150, list.tail.prev.value);
         assertEquals(200, list.tail.value);
-
+        list.add(300); //new tail
+        assertEquals(6, list.count());
+        assertEquals(10, list.head.value);
+        assertEquals(100, list.head.next.value);
+        assertEquals(120, list.head.next.next.value);
+        assertEquals(150, list.tail.prev.prev.value);
+        assertEquals(200, list.tail.prev.value);
+        assertEquals(300, list.tail.value);
     }
 
     @Test
     void testAdd_Desc() {
-        OrderedList<Integer> list = new OrderedList<>(true);
+        OrderedList<Integer> list = new OrderedList<>(false);
         list.add(100);
         assertEquals(1, list.count());
         assertEquals(100, list.head.value);
         assertEquals(100, list.tail.value);
-        list.add(200);
+        list.add(200); //new head
         assertEquals(2, list.count());
         assertEquals(200, list.head.value);
         assertEquals(100, list.tail.value);
@@ -54,7 +72,7 @@ class OrderedListTest {
         assertEquals(200, list.head.value);
         assertEquals(150, list.head.next.value);
         assertEquals(100, list.tail.value);
-        list.add(10);
+        list.add(10); //new tail
         assertEquals(4, list.count());
         assertEquals(200, list.head.value);
         assertEquals(150, list.head.next.value);
@@ -67,25 +85,125 @@ class OrderedListTest {
         assertEquals(120, list.head.next.next.value);
         assertEquals(100, list.tail.prev.value);
         assertEquals(10, list.tail.value);
+        list.add(300); //new head
+        assertEquals(6, list.count());
+        assertEquals(300, list.head.value);
+        assertEquals(200, list.head.next.value);
+        assertEquals(150, list.head.next.next.value);
+        assertEquals(120, list.head.next.next.next.value);
+        assertEquals(100, list.tail.prev.value);
+        assertEquals(10, list.tail.value);
     }
 
     @Test
-    void testFind() {
+    void testFind_Asc() {
+        OrderedList<Integer> list = new OrderedList<>(true);
+        assertNull(list.find(100));
+        list.add(100);
+        assertEquals(100, list.find(100).value);
+        list.add(200);
+        list.add(300);
+        list.add(400);
+        list.add(500);
+        assertEquals(100, list.find(100).value);
+        assertEquals(400, list.find(400).value);
+        assertEquals(500, list.find(500).value);
+        assertNull(list.find(150));
+        assertNull(list.find(50));
+        assertNull(list.find(600));
+    }
+
+    @Test
+    void testFind_Desc() {
+        OrderedList<Integer> list = new OrderedList<>(false);
+        assertNull(list.find(100));
+        list.add(100);
+        assertEquals(100, list.find(100).value);
+        list.add(200);
+        list.add(300);
+        list.add(400);
+        list.add(500);
+        assertEquals(100, list.find(100).value);
+        assertEquals(400, list.find(400).value);
+        assertEquals(500, list.find(500).value);
+        assertNull(list.find(150));
+        assertNull(list.find(50));
+        assertNull(list.find(600));
     }
 
     @Test
     void testDelete_Asc() {
+        OrderedList<Integer> list = new OrderedList<>(true);
+        list.delete(100);
+        list.add(100);
+        assertEquals(1, list.count());
+        list.delete(100);
+        assertEquals(0, list.count());
+        list.add(100);
+        list.add(200);
+        list.delete(100);
+        assertEquals(1, list.count());
+        assertEquals(200, list.head.value);
+        assertEquals(200, list.tail.value);
+        list.add(100);
+        list.add(150);
+        list.add(300);
+        list.add(150);
+        list.delete(150);
+        assertEquals(4, list.count());
+        assertEquals(100, list.head.value);
+        assertEquals(150, list.head.next.value);
+        assertEquals(200, list.tail.prev.value);
+        assertEquals(300, list.tail.value);
     }
 
     @Test
     void testDelete_Desc() {
+        OrderedList<Integer> list = new OrderedList<>(false);
+        list.delete(100);
+        assertEquals(0, list.count());
+        list.add(100);
+        list.delete(100);
+        assertEquals(0, list.count());
+        list.add(100);
+        list.add(200);
+        list.delete(100);
+        assertEquals(1, list.count());
+        assertEquals(200, list.head.value);
+        assertEquals(200, list.tail.value);
+        list.add(100);
+        list.add(150);
+        list.add(300);
+        list.add(150);
+        assertEquals(5, list.count());
+        list.delete(150);
+        assertEquals(4, list.count());
+        assertEquals(300, list.head.value);
+        assertEquals(200, list.head.next.value);
+        assertEquals(150, list.tail.prev.value);
+        assertEquals(100, list.tail.value);
     }
 
     @Test
     void clear() {
+        OrderedList<Integer> list = new OrderedList<>(true);
+        list.clear(true);
+        assertNull(list.head);
+        assertNull(list.tail);
+        assertEquals(0, list.count());
+        list.add(1);
+        assertEquals(1, list.count());
+        list.clear(true);
+        assertNull(list.head);
+        assertNull(list.tail);
+        assertEquals(0, list.count());
+        list.add(100);
+        list.add(500);
+        assertEquals(2, list.count());
+        list.clear(true);
+        assertNull(list.head);
+        assertNull(list.tail);
+        assertEquals(0, list.count());
     }
 
-    @Test
-    void count() {
-    }
 }
