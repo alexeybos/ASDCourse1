d75ffb7629686783bb616403273d1b60e76b4db7
Alexey.Bosalyga <alexey.bosalyga@billing.ru>
2025-02-07 11:22:59 +0400

    lesson7

---
+++ src/main/java/org/skillsmart/asd2real/lesson7/Heap.java
@@ -0,0 +1,120 @@
+package org.skillsmart.asd2real.lesson7;
+
+import java.util.*;
+
+class Heap
+{
+    public int [] HeapArray;
+    private int lastInHeap;
+
+    public Heap() {
+        HeapArray = null;
+        lastInHeap = -1;
+    }
+
+    public void MakeHeap(int[] a, int depth)
+    {
+        int heapSize = (int) Math.pow(2, depth + 1) - 1;
+        HeapArray = new int[heapSize];
+        for (int key : a) {
+            Add(key);
+        }
+    }
+
+    public int GetMax()
+    {
+        if (lastInHeap == -1) return -1;
+        int max = HeapArray[0];
+        HeapArray[0] = HeapArray[lastInHeap];
+        HeapArray[lastInHeap] = 0;
+        lastInHeap--;
+        checkChildren(0);
+        return max;
+    }
+
+    private void checkChildren(int i) {
+        int childIndex = (2 * i + 1);
+        if (childIndex <= lastInHeap && HeapArray[childIndex] > HeapArray[i]) {
+            swap(childIndex, i);
+            checkChildren(childIndex);
+        }
+        childIndex++;
+        if (childIndex <= lastInHeap && HeapArray[childIndex] > HeapArray[i]) {
+            swap(childIndex, i);
+            checkChildren(childIndex);
+        }
+    }
+
+    public boolean Add(int key)
+    {
+        if (lastInHeap + 1 == HeapArray.length) return false;
+        lastInHeap++;
+        HeapArray[lastInHeap] = key;
+        for (int i = lastInHeap; i > 0; i = (i - 1) / 2) {
+            int parentIndex = (i - 1) / 2;
+            if (HeapArray[parentIndex] < HeapArray[i]) {
+                swap(parentIndex, i);
+            } else {
+                int childIndex = (2 * i + 1);
+                if (childIndex <= lastInHeap && HeapArray[childIndex] > key) {
+                    swap(childIndex, i);
+                    return true;
+                }
+                childIndex++;
+                if (childIndex <= lastInHeap && HeapArray[childIndex] > key) {
+                    swap(childIndex, i);
+                    return true;
+                }
+                return true;
+            }
+        }
+        return true;
+    }
+
+    private void swap(int index1, int index2) {
+        int tmpForSwap = HeapArray[index2];
+        HeapArray[index2] = HeapArray[index1];
+        HeapArray[index1] = tmpForSwap;
+    }
+
+    //TODO дополнительные задания
+
+    //Сложность: time O(n), память O(depth)
+    public boolean isCorrect() {
+        if (lastInHeap < 2) return true;
+        return isChildrenCorrect(0);
+    }
+
+    private boolean isChildrenCorrect(int i) {
+        if (i > lastInHeap) return true;
+        int nextChild = i * 2 + 1;
+        if (nextChild > lastInHeap) return true;
+        if (HeapArray[nextChild] > HeapArray[i]) return false;
+        boolean checkChild = isChildrenCorrect(nextChild);
+        nextChild++;
+        if (checkChild && nextChild > lastInHeap) return true;
+        if (checkChild && HeapArray[nextChild] < HeapArray[i]) return isChildrenCorrect(nextChild);
+        return false;
+    }
+
+    //Сложность: time O(n), память O(1)
+    public int findMaxInRange(int start, int end) {
+        if (lastInHeap == -1) return -1;
+        if (HeapArray[0] < start) return -1;
+        int max = -1;
+        for (int i = 0; i <= lastInHeap; i++) {
+            if (HeapArray[i] >= start && HeapArray[i] <= end && HeapArray[i] > max) max = HeapArray[i];
+        }
+        return max;
+    }
+
+    //Сложность: time O(n*logn), память O(depth)
+    public void union(Heap heap) {
+        for (int i = heap.GetMax(); i != -1; i = heap.GetMax()) {
+            Add(i);
+        }
+    }
+}
+
+
+
---
+++ src/test/java/org/skillsmart/asd2real/lesson7/HeapTest.java
@@ -0,0 +1,249 @@
+package org.skillsmart.asd2real.lesson7;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HeapTest {
+
+    @Test
+    void testMakeHeap_EmptyOrOneRootHeap() {
+        Heap heap = new Heap();
+        int[] arr = new int[0];
+        heap.MakeHeap(arr, 0);
+        assertEquals(1, heap.HeapArray.length);
+        assertEquals(0, heap.HeapArray[0]);
+        arr = new int[]{8};
+        heap.MakeHeap(arr, 0);
+        assertEquals(1, heap.HeapArray.length);
+        assertEquals(8, heap.HeapArray[0]);
+    }
+
+    @Test
+    void testMakeHeap_MakeEmptyHeap() {
+        Heap heap = new Heap();
+        int[] arr = new int[0];
+        heap.MakeHeap(arr, 2);
+        assertEquals(7, heap.HeapArray.length);
+    }
+
+    @Test
+    void testMakeHeap() {
+        Heap heap = new Heap();
+        int[] arr = new int[]{8, 4, 12};
+        heap.MakeHeap(arr, 1);
+        assertEquals(3, heap.HeapArray.length);
+        assertEquals(12, heap.HeapArray[0]);
+        assertEquals(4, heap.HeapArray[1]);
+        assertEquals(8, heap.HeapArray[2]);
+
+        heap = new Heap();
+        arr = new int[]{8, 4, 12, 2, 6, 14, 1, 3, 7, 5, 9, 11, 15};
+        heap.MakeHeap(arr, 3);
+        assertEquals(15, heap.HeapArray[0]);
+        assertEquals(0, heap.HeapArray[14]);
+        assertEquals(0, heap.HeapArray[13]);
+        assertEquals(11, heap.HeapArray[12]);
+    }
+
+    @Test
+    void testGetMax_EmptyOrOneRootHeap() {
+        Heap heap = new Heap();
+        int[] arr = new int[0];
+        heap.MakeHeap(arr, 0);
+        assertEquals(-1, heap.GetMax());
+        arr = new int[]{8};
+        heap.MakeHeap(arr, 0);
+        assertEquals(8, heap.GetMax());
+        assertEquals(-1, heap.GetMax());
+    }
+
+    @Test
+    void testGetMax() {
+        Heap heap = new Heap();
+        int[] arr = new int[]{8, 4, 12, 2, 6, 14, 1, 3, 7, 5, 9, 11, 15};
+        heap.MakeHeap(arr, 3);
+        assertEquals(15, heap.GetMax());
+        assertEquals(14, heap.HeapArray[0]);
+        assertEquals(14, heap.GetMax());
+        assertEquals(12, heap.GetMax());
+        assertEquals(11, heap.GetMax());
+        assertEquals(9, heap.GetMax());
+        assertEquals(8, heap.GetMax());
+        assertEquals(7, heap.GetMax());
+        assertEquals(6, heap.GetMax());
+        assertEquals(5, heap.GetMax());
+        assertEquals(4, heap.GetMax());
+        assertEquals(3, heap.GetMax());
+        assertEquals(2, heap.GetMax());
+        assertEquals(1, heap.GetMax());
+        assertEquals(-1, heap.GetMax());
+    }
+
+    @Test
+    void testGetMax_WithAdd() {
+        Heap heap = new Heap();
+        int[] arr = new int[]{10, 5, 8};
+        heap.MakeHeap(arr, 2);
+        assertTrue(heap.Add(4));
+        assertTrue(heap.Add(1));
+        assertEquals(10, heap.HeapArray[0]);
+        assertEquals(5, heap.HeapArray[1]);
+        assertEquals(8, heap.HeapArray[2]);
+        assertEquals(4, heap.HeapArray[3]);
+        assertEquals(1, heap.HeapArray[4]);
+        assertEquals(10, heap.GetMax());
+        assertEquals(8, heap.HeapArray[0]);
+    }
+
+    @Test
+    void testAdd_EmptyHeap() {
+        Heap heap = new Heap();
+        int[] arr = new int[0];
+        heap.MakeHeap(arr, 0);
+        assertTrue(heap.Add(8));
+        assertFalse(heap.Add(10));
+    }
+
+    @Test
+    void testAdd() {
+        Heap heap = new Heap();
+        int[] arr = new int[]{8, 4, 12, 2, 6, 14, 1, 3, 7, 5, 9, 11, 15};
+        heap.MakeHeap(arr, 3);
+        assertTrue(heap.Add(10));
+        assertTrue(heap.Add(13));
+        assertFalse(heap.Add(20));
+        for (int i = 0; i < heap.HeapArray.length; i++) {
+            assertTrue(heap.HeapArray[i] != 0);
+        }
+    }
+
+    @Test
+    void testIsCorrect_EmptyOrOneElemHeap() {
+        Heap heap = new Heap();
+        int[] arr = new int[0];
+        heap.MakeHeap(arr, 0);
+        assertTrue(heap.isCorrect());
+        heap.Add(8);
+        assertTrue(heap.isCorrect());
+    }
+
+    @Test
+    void testIsCorrect_Positive() {
+        Heap heap = new Heap();
+        int[] arr = new int[]{8, 4, 12, 2, 6, 14, 1, 3, 7, 5, 9, 11, 15};
+        heap.MakeHeap(arr, 3);
+        assertTrue(heap.isCorrect());
+        assertTrue(heap.Add(10));
+        assertTrue(heap.isCorrect());
+        assertTrue(heap.Add(13));
+        assertTrue(heap.isCorrect());
+        assertFalse(heap.Add(20));
+        assertTrue(heap.isCorrect());
+    }
+
+    @Test
+    void testIsCorrect_Negative() {
+        Heap heap = new Heap();
+        int[] arr = new int[]{8, 4, 12, 2, 6, 14, 1, 3, 7, 5, 9, 11, 15};
+        heap.MakeHeap(arr, 3);
+        assertTrue(heap.isCorrect());
+        int tmp = heap.HeapArray[4];
+        heap.HeapArray[4] = heap.HeapArray[10];
+        heap.HeapArray[10] = tmp;
+        assertFalse(heap.isCorrect());
+    }
+
+    @Test
+    void testFindMaxInRange_EmptyOrOneNodeHeap() {
+        Heap heap = new Heap();
+        int[] arr = new int[0];
+        heap.MakeHeap(arr, 0);
+        assertEquals(-1, heap.findMaxInRange(1, 10));
+        heap.Add(8);
+        assertEquals(8, heap.findMaxInRange(1, 10));
+        assertEquals(-1, heap.findMaxInRange(9, 50));
+        assertEquals(-1, heap.findMaxInRange(1, 7));
+    }
+
+    @Test
+    void testFindMaxInRange() {
+        Heap heap = new Heap();
+        int[] arr = new int[]{8, 4, 12, 2, 6, 14, 1, 3, 7, 5, 9, 11, 15};
+        heap.MakeHeap(arr, 3);
+        assertEquals(15, heap.findMaxInRange(10, 20));
+        assertEquals(-1, heap.findMaxInRange(10, 10));
+        assertEquals(9, heap.findMaxInRange(5, 10));
+    }
+
+    @Test
+    void testUnion_TwoEmpty() {
+        Heap heap = new Heap();
+        int[] arr = new int[0];
+        heap.MakeHeap(arr, 0);
+        Heap heap2 = new Heap();
+        int[] arr2 = new int[0];
+        heap2.MakeHeap(arr2, 0);
+        heap.union(heap2);
+        assertEquals(1, heap.HeapArray.length);
+        assertEquals(0, heap.HeapArray[0]);
+    }
+
+    @Test
+    void testUnion_LeftEmpty() {
+        Heap heap = new Heap();
+        int[] arr = {8, 4};
+        heap.MakeHeap(arr, 1);
+        Heap heap2 = new Heap();
+        int[] arr2 = new int[0];
+        heap2.MakeHeap(arr2, 1);
+        heap.union(heap2);
+        assertEquals(3, heap.HeapArray.length);
+        assertEquals(8, heap.HeapArray[0]);
+        assertEquals(4, heap.HeapArray[1]);
+        assertEquals(0, heap.HeapArray[2]);
+    }
+
+    @Test
+    void testUnion_RightEmpty() {
+        Heap heap = new Heap();
+        int[] arr = {8, 4};
+        heap.MakeHeap(arr, 1);
+        Heap heap2 = new Heap();
+        int[] arr2 = new int[0];
+        heap2.MakeHeap(arr2, 1);
+        heap2.union(heap);
+        assertEquals(3, heap2.HeapArray.length);
+        assertEquals(8, heap2.HeapArray[0]);
+        assertEquals(4, heap2.HeapArray[1]);
+        assertEquals(0, heap2.HeapArray[2]);
+    }
+
+    @Test
+    void testUnion() {
+        Heap heap = new Heap();
+        int[] arr = {8, 4};
+        heap.MakeHeap(arr, 3);
+        Heap heap2 = new Heap();
+        int[] arr2 = new int[]{12, 2, 6, 14, 1, 3, 7, 5, 9, 11, 15};
+        heap2.MakeHeap(arr2, 3);
+        heap.union(heap2);
+        assertEquals(15, heap.HeapArray[0]);
+        assertTrue(heap.isCorrect());
+        assertEquals(15, heap.GetMax());
+        assertEquals(14, heap.HeapArray[0]);
+        assertEquals(14, heap.GetMax());
+        assertEquals(12, heap.GetMax());
+        assertEquals(11, heap.GetMax());
+        assertEquals(9, heap.GetMax());
+        assertEquals(8, heap.GetMax());
+        assertEquals(7, heap.GetMax());
+        assertEquals(6, heap.GetMax());
+        assertEquals(5, heap.GetMax());
+        assertEquals(4, heap.GetMax());
+        assertEquals(3, heap.GetMax());
+        assertEquals(2, heap.GetMax());
+        assertEquals(1, heap.GetMax());
+        assertEquals(-1, heap.GetMax());
+    }
+}
